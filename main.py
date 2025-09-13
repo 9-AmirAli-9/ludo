af1e26ecb6ce6233feb5cead9824be5831248986
thispc <khoddamamirali841@gmail.com>
2025-09-13 18:15:50 +0330

    taking turns

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+
 def main():
      print('welcome to my ludo game.')
-
      player=[]
      n=int(input('how many player?:'))
      i=1
@@ -8,23 +9,29 @@ def main():
      while i <=n:
           if i==1:
                color=input('enter player color[red,yellow,blue,green] : ')
-               player1={'color':color,'turn':0,'in':0}
+               player1={'color':color,'block':0,'in':0}
                player.append(player1)
           elif i==2:
                color=input('enter player color[red,yellow,blue,green] : ')
-               player2={'color':color,'turn':0,'in':0}
+               player2={'color':color,'block':0,'in':0}
                player.append(player2)
           elif i==3:
                color=input('enter player color[red,yellow,blue,green] : ')
-               player3={'color':color,'turn':0,'in':0}
+               player3={'color':color,'block':0,'in':0}
                player.append(player3)
           elif i==4:
                color=input('enter player color[red,yellow,blue,green] : ')
-               player4={'color':color,'turn':0,'in':0}
+               player4={'color':color,'block':0,'in':0}
                player.append(player4)
           i+=1
 
      running=True
-     while running :
-          pass
+     while running:
+          round=0
+          i=round%n
+          tass=random.randrange(1,7)
+          if player[i]['in']==1:
+               player[i]['block']+=tass
+          
+
 
